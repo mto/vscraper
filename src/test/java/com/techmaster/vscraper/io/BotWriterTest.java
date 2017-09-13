05ebc0c8aafddf314c9df726eb6a33174168b149
Minh Hoang TO <hoang281283@gmail.com>
2017-09-13 20:11:31 +0700

    Complete BotWriterTest.java

--- src/test/java/com/techmaster/vscraper/io/BotWriterTest.java
+++ src/test/java/com/techmaster/vscraper/io/BotWriterTest.java
@@ -2,7 +2,14 @@ package com.techmaster.vscraper.io;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * @author <a href="hoang281283@gmail.com">Minh Hoang TO</a>
@@ -17,5 +24,15 @@ public class BotWriterTest {
         bw.streamToFile(is, "/s2f.txt");
 
         //TODO: Add assert statement to compare the content of s2f.txt file with '123456789'
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Path src = new File("target/s2f.txt").toPath();
+
+        try {
+            Files.copy(src, out);
+
+            assertEquals("123456789", new String(out.toByteArray()));
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
     }
 }
